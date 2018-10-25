5d7bf6fa6b530a0c69e08a81ee99cdcb3ac97bb2
huiren <zhrlnt@gmail.com>
2018-10-25 21:27:55 +0800

    config

--- foundation/application.go
+++ foundation/application.go
@@ -1,6 +1,7 @@
 package foundation
 
 import (
+	"github.com/Kretech/contracts/config"
 	"github.com/Kretech/framework/event"
 	"github.com/Kretech/scaffold/container"
 )
@@ -9,13 +10,15 @@ var app *Application
 
 type Application struct {
 	container.Container
+
+	Conf config.Config
 }
 
 func App() *Application {
 
 	if app == nil {
 		app = &Application{
-			*container.NewContainer(),
+			Container: *container.NewContainer(),
 		}
 
 		app.registerBaseServiceProviders()
@@ -26,13 +29,20 @@ func App() *Application {
 }
 
 func (app *Application) registerBaseServiceProviders() {
-	app.registerServiceProvider(&event.ServiceProvider{})
+	//app.RegisterServiceProvider(&configImpl.TomlServiceProvider{})
+	//app.SetConfig(app.Make("config").(config.Config))
+
+	app.RegisterServiceProvider(&event.ServiceProvider{})
 }
 
 func (app *Application) registerCoreContainerAlias() {
 
 }
 
-func (app *Application) registerServiceProvider(provider ServiceProvider) {
+func (app *Application) RegisterServiceProvider(provider ServiceProvider) {
 	provider.Register(&app.Container)
+}
+
+func (app *Application) SetConfig(conf config.Config) {
+	app.Conf = conf
 }
